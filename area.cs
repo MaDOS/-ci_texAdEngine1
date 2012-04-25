6e1257e5f3b1880e8280f342ef3690ea01551b9f
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 21:58:18 +0200

    - implemented possibility to save maps local

--- area.cs
+++ area.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace ci_texAdEngine1
@@ -6,16 +7,47 @@ namespace ci_texAdEngine1
 	public class area
 	{
 		Dictionary<int, item> items;
-				
+		IniFile dataIni;
 		int id;
 		
 		public area(string name)
 		{
-			IniFile dataIni = new IniFile(game.root + "\\maps\\" + name + ".ini");
+			if(File.Exists(game.root + "\\saves\\maps\\" + name + ".ini"))
+			{
+				dataIni = new IniFile(game.root + "\\saves\\maps\\" + name + ".ini")
+			}
+			else
+			{
+				try
+				{
+					dataIni = new IniFile(game.root + "\\maps\\" + name + ".ini")
+				}
+				catch(Exception ex)
+				{
+					throw ex; 
+				}
+			}
 			items = new Dictionary<int, item>();
 //			Load items from a save-ini
 			
 			
 		}
+		
+		public void save()
+		{
+			string itemsString = "";
+			
+			foreach(item i in items)
+			{
+				itemsString += i.filename + ",";
+			}
+			
+			itemsString = itemsString.Substring(0, itemsString.Length - 1); //trim last comma away
+			
+			dataIni.AddSetting("content", "items", itemsString);
+			
+			dataIni.SaveSettings(game.root + "\\saves\\maps\\" + name + ".ini");
+		}
 	}
 }
+"
