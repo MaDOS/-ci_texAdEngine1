073ca8726ff7a6cdb36cd1bdb21b4ab123569685
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 20:44:57 +0200

    - debugging - base structure

--- player.cs
+++ player.cs
@@ -7,11 +7,18 @@ namespace ci_texAdEngine1
 	public class player
 	{
 		public Dictionary<int, item> inventory;
+		private area position;
 		
 		public player(string playername)
 		{
-			if(File.Exists(game.root + "\\saves\\players\\" + playername + ".ini"));
+			if(File.Exists(game.root + "\\saves\\players\\" + playername + ".ini"))
+			{
 				IniFile dataIni = new IniFile(game.root + "\\saves\\players\\" + playername + ".ini");
+			}
+			else
+			{
+				position = game.spawn;
+			}
 			
 			inventory = new Dictionary<int, item>();
 //			Load inventory from a save-ini
@@ -19,6 +26,11 @@ namespace ci_texAdEngine1
 		}
 		
 		public void save()
+		{
+			
+		}
+		
+		public void go(string direction)
 		{
 			
 		}
