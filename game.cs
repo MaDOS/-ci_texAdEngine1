7b21f7245904e2422f9067118961e7ed78877547
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 20:00:50 +0200

    - added game base structure

--- game.cs
+++ game.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using ci_texAdEngine1;
 
 namespace ci_texAdEngine1
 {
 	public class game
 	{
-		map map;
-		area[] activeAreas;
+//		public Dictionary<int,item> items;
+		public static string root = Path.GetDirectoryName(Assembly.GetAssembly(typeof(game)).CodeBase);
+		private area[] activeAreas;
 		
-		public game(map map_param)
+		public game(area startMap)
 		{
+//			items = new Dictionary<int,item>();
 		}
 	}
 }
