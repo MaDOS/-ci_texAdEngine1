5f12c1de0a6c9bfd81688776a245d9ea2956b766
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 20:00:30 +0200

    - added are-class base structure

--- area.cs
+++ area.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ci_texAdEngine1
 {
 	public class area
 	{
+		Dictionary<int, item> items;
+				
+		int id;
+		
 		public area(string name)
 		{
+			IniFile dataIni = new IniFile(game.root + "\\maps\\" + name + ".ini");
+			items = new Dictionary<int, item>();
+//			Load items from a save-ini
+			
+			
 		}
 	}
 }
