8aac6c39b4265f52b2a6fe36a597bfa6871dfda7
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 20:00:02 +0200

    - aded player class base structure

---
+++ player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ci_texAdEngine1
+{
+	public class player
+	{
+		public Dictionary<int, item> inventory;
+		
+		public player(string playername)
+		{
+			if(File.Exists(game.root + "\\saves\\players\\" + playername + ".ini"));
+				IniFile dataIni = new IniFile(game.root + "\\saves\\players\\" + playername + ".ini");
+			
+			inventory = new Dictionary<int, item>();
+//			Load inventory from a save-ini
+			
+		}
+		
+		public void save()
+		{
+			
+		}
+	}
+}
