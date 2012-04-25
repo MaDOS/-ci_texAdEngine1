dd6bde5231bccade5ac8fde04a8e7bfa47ce3fc9
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-25 20:43:55 +0200

    - realized that I don't need to keep track of every item and area active

--- game.cs
+++ game.cs
@@ -8,13 +8,14 @@ namespace ci_texAdEngine1
 {
 	public class game
 	{
-//		public Dictionary<int,item> items;
 		public static string root = Path.GetDirectoryName(Assembly.GetAssembly(typeof(game)).CodeBase);
-		private area[] activeAreas;
+		public static area spawn;
+//		private area[] activeAreas;
 		
 		public game(area startMap)
 		{
-//			items = new Dictionary<int,item>();
+			spawn = startMap;
+//			activeAreas += spawn;
 		}
 	}
 }
