b2300b9f88159ad05d2cbff8d5ffbdb3e58c4078
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-27 18:32:13 +0200

    - readded id property and constructor overload to construct an item by only having the id

--- item.cs
+++ item.cs
@@ -9,6 +9,7 @@ namespace ci_texAdEngine1
 	{
 		public string filename;
 		public string name;
+		public int id;
 		public bool takeable;
 		public bool useable;
 		public bool weapon;
@@ -24,6 +25,28 @@ namespace ci_texAdEngine1
 		{
 			dataIni = new IniFile(game.root + "\\items\\" + filename);
 			
+			id = Convert.ToInt32(dataIni.GetSetting("identification", "id"));
+			filename = dataIni.GetSetting("identification", "filename");
+			name = dataIni.GetSetting("identification", "name");
+			
+			takeable = Convert.ToBoolean(dataIni.GetSetting("flags", "takeable"));
+			useable = Convert.ToBoolean(dataIni.GetSetting("flags", "useable"));
+			weapon = Convert.ToBoolean(dataIni.GetSetting("flags", "weapon"));
+			key = Convert.ToBoolean(dataIni.GetSetting("flags", "key"));
+			eatable = Convert.ToBoolean(dataIni.GetSetting("flags", "eatable"));
+			
+			damage = Convert.ToInt32(dataIni.GetSetting("properties", "damage"));
+			heal = Convert.ToInt32(dataIni.GetSetting("properties", "heal"));
+			weight = Convert.ToDouble(dataIni.GetSetting("properties", "weight"));
+		}
+		
+		public item(int id)
+		{
+			string filename = new IniFile(game.root + "\\items\\ids.ini").GetSetting("ids", id.ToString());
+			
+			dataIni = new IniFile(game.root + "\\items\\" + filename);
+			
+			id = Convert.ToInt32(dataIni.GetSetting("identification", "id"));
 			filename = dataIni.GetSetting("identification", "filename");
 			name = dataIni.GetSetting("identification", "name");
 			
