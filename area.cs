251fa34f2aa0d9592655537b23c302b8ddd23a06
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-28 01:54:26 +0200

    - added missing attributes and done some debugging

--- area.cs
+++ area.cs
@@ -11,10 +11,11 @@ namespace ci_texAdEngine1
 		public int id;
 		private string filename;
 		private string name;
+		private string map;
 		
 		public area(string filename)
 		{
-			string savedMap = game.root + "\\saves\\maps\\" + filename;
+			string savedMap = game.root + "\\saves\\maps\\" + map + "\\" + filename;
 			
 			if(File.Exists(savedMap))
 			{
@@ -22,34 +23,25 @@ namespace ci_texAdEngine1
 			}
 			else
 			{
-				try
-				{
-					dataIni = new IniFile(game.root + "\\maps\\" + filename);
-				}
-				catch(Exception ex)
-				{
-					throw ex; 
-				}
+				dataIni = new IniFile(game.root + "\\maps\\" + map + "\\" + filename);
 			}
 			
+			id = Convert.ToInt32(dataIni.GetSetting("identification", "id"));
 			filename = dataIni.GetSetting("identification", "filename");
 			name = dataIni.GetSetting("identification", "name");
+			map = dataIni.GetSetting("identification", "map");
 			
 			drops = new Dictionary<int, int>();
 			
 			string serializedItems = dataIni.GetSetting("content", "drops");
 			string[] serializedPairs = serializedItems.Split(',');
 			string[] splittedPair = "";
+			
 			foreach(string pair in serializedPairs)
 			{
 				splittedPair = pair.Split(':');
 				drops.Add(Convert.ToInt32(splittedPair[0]), Convert.ToInt32(splittedPair[1]));
 			}
-		}
-		
-		public void spawnPlayer(string name)
-		{
-//			game.activePlayers.Add(name, new player(name, this));
 		}
 		
 		public void save()
@@ -64,6 +56,8 @@ namespace ci_texAdEngine1
 			serializedItems = serializedItems.Substring(0, serializedItems.Length - 1); //trim last comma away
 			
 			dataIni.AddSetting("content", "drops", serializedItems);
+			
+			dataIni.SaveSettings(game.root + "\\saves\\maps\\" + map + "\\" + filename);
 		}
 	}
 }
