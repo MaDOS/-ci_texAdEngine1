41543fc2ff71b97102bf5b3c38f57f3d9e2a30d5
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-28 01:18:45 +0200

    - areas and items are now preloaded on startup and kept as instance associated to id in ram while runtime - added onConnect.. just bassically I will change this later

--- game.cs
+++ game.cs
@@ -11,29 +11,81 @@ namespace ci_texAdEngine1
 	{
 		public static string root = Path.GetDirectoryName(Assembly.GetAssembly(typeof(game)).CodeBase);
 		public static int itemsCount = new IniFile(root + "\\items\\ids.ini").keyPairs.Count;
-		public static Dictionary<string, area> activeAreas;
-		public static Dictionary<string, player> activePlayers;
+		public string map;
+		public Dictionary<int, item> items; //id, instance
+		public Dictionary<int, area> areas; //filename, instance
+		public Dictionary<string, player> activePlayers;
 		
-		public game(string spawnAreaFileName)
+		public game(string map_param)
 		{
+			StreamReader rdr = new StreamReader(root + "\\items\\ids.ini");
+			string line = rdr.ReadLine();
+			while(line != "")
+			{
+				line = rdr.ReadLine();
+				if(line.ToCharArray()[0] == '#' 
+				   || line.ToCharArray()[0] == '\'' 
+				   || line.ToCharArray()[0] == '[')
+				{
+					//comment or section-identifier
+				}
+				else
+				{
+					string[] splitLine = line.Split('=');
+					items.Add(Convert.ToInt32(splitLine[0]), new item(splitLine[1]));
+				}
+			}
+			rdr.Close();
+			
+			rdr = new StreamReader(root + "\\maps\\" + map + "\\")
+			string line = rdr.ReadLine();
+			while(line != "")
+			{
+				line = rdr.ReadLine();
+				if(line.ToCharArray()[0] == '#' 
+				   || line.ToCharArray()[0] == '\'' 
+				   || line.ToCharArray()[0] == '[')
+				{
+					//comment or section-identifier
+				}
+				else
+				{
+					string[] splitLine = line.Split('=');
+					areas.Add(Convert.ToInt32(splitLine[0], new area(splitLine[1]));
+				}
+			}
+			rdr.Close();
+			
 			activePlayers = new Dictionary<string, player>();
 		}
 		
-		public string onConnect(string playername, string password)
+		public string onConnect(string playername, string password) //first connection handling and password protecting are a bit messed up and very unsafe. I will change this later
 		{
 			string saveFile = root + "\\saves\\players\\" + playername + ".ini";
 			
-			ci_crypter.encodeFile(saveFile, 139);
-			IniFile playerDataIni = new IniFile(saveFile);
-			ci_crypter.decodeFile(saveFile, 139);
-				
-			//check password
-			if(password != playerDataIni.GetSetting("identification", "password"))
+			if(File.Exists(saveFile))
 			{
-				return "incorrect password!";
-			}
 			
-			activeAreas[playerDataIni.GetSetting("attributes", "position")].spawnPlayer(playername);
+				ci_crypter.encodeFile(saveFile, 139);
+				IniFile playerDataIni = new IniFile(saveFile);
+				ci_crypter.decodeFile(saveFile, 139);
+					
+				//check password
+				if(password != playerDataIni.GetSetting("identification", "password"))
+				{
+					return "incorrect password!";
+				}
+				
+				activePlayers.Add(playername, new player(playername, areas[Convert.ToInt32(playerDataIni.GetSetting("attributes", "area"))]));
+			}
+			else
+			{
+				IniFile playerDataIni = new IniFile(saveFile);
+				playerDataIni.AddSetting("identification", "password", password);
+				playerDataIni.SaveSettings();
+				ci_crypter.decodeFile(saveFile, 139);
+				activePlayers.Add(playername, new player(playername, areas[0]));
+			}
 			
 			return "connected successfully!";
 		}
