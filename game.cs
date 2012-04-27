0749ec87cbf4f9285ae290093aba2df95b44d0d7
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-27 18:30:33 +0200

    - game.cs does now handle both aticve areas and players

--- game.cs
+++ game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using ci_;
 using System.Reflection;
 using ci_texAdEngine1;
 
@@ -9,13 +10,32 @@ namespace ci_texAdEngine1
 	public class game
 	{
 		public static string root = Path.GetDirectoryName(Assembly.GetAssembly(typeof(game)).CodeBase);
-		public static area spawn;
-//		private area[] activeAreas;
+		public static int itemsCount = new IniFile(root + "\\items\\ids.ini").keyPairs.Count;
+		public static Dictionary<string, area> activeAreas;
+		public static Dictionary<string, player> activePlayers;
 		
-		public game(area startMap)
+		public game(string spawnAreaFileName)
 		{
-			spawn = startMap;
-//			activeAreas += spawn;
+			activePlayers = new Dictionary<string, player>();
+		}
+		
+		public string onConnect(string playername, string password)
+		{
+			string saveFile = root + "\\saves\\players\\" + playername + ".ini";
+			
+			ci_crypter.encodeFile(saveFile, 139);
+			IniFile playerDataIni = new IniFile(saveFile);
+			ci_crypter.decodeFile(saveFile, 139);
+				
+			//check password
+			if(password != playerDataIni.GetSetting("identification", "password"))
+			{
+				return "incorrect password!";
+			}
+			
+			activeAreas[playerDataIni.GetSetting("attributes", "position")].spawnPlayer(playername);
+			
+			return "connected successfully!";
 		}
 	}
 }
