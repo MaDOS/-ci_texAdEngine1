20fae94b136204e64abc924e6ff80d40709a73a4
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-27 19:38:58 +0200

    - änderungen

--- player.cs
+++ player.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using ci_;
 using System.Collections.Generic;
 
 namespace ci_texAdEngine1
@@ -7,32 +7,67 @@ namespace ci_texAdEngine1
 	public class player
 	{
 		public Dictionary<int, item> inventory;
+		public string name;
+		private IniFile dataIni;
+		private string filename;
+		private string saveFile;
 		private area position;
 		
-		public player(string playername)
+		public player(string playername, area pos)
 		{
-			if(File.Exists(game.root + "\\saves\\players\\" + playername + ".ini"))
+			saveFile = game.root + "\\saves\\players\\" + playername + ".ini";
+			
+			inventory = new Dictionary<int, item>();
+			
+			if(File.Exists(saveFile))
 			{
-				IniFile dataIni = new IniFile(game.root + "\\saves\\players\\" + playername + ".ini");
+				//player exists
+				ci_crypter.encodeFile(saveFile, 139);
+				dataIni = new IniFile(saveFile);
+				ci_crypter.decodeFile(saveFile, 139);
+
+				
+				//inventory
+				string tempItemString = dataIni.GetSetting("inventory", "items");
+				
+				foreach(string itemFilename in tempItemString.Split(','))
+				{
+					inventory.Add(inventory.Count, new item(itemFilename));
+				}
 			}
 			else
 			{
-				position = game.spawn;
+				//player is new on the server
+				filename = playername + ".ini";
+				name = playername;
+				
 			}
 			
-			inventory = new Dictionary<int, item>();
-//			Load inventory from a save-ini
+			position = pos;
 			
+			
+		}
+		
+		private void takeItem(string name)
+		{
+			
+		}
+		
+		private void dropItem(int id)
+		{
+			int itemCount = 0;
+			
+			foreach(item in position.items[id])
+			{
+				itemCount++;
+			}
+			
+			position.items[id][itemCount - 1] = new item(id);
 		}
 		
 		public void save()
 		{
-			
-		}
-		
-		public void go(string direction)
-		{
-			
+			dataIni.SaveSettings();
 		}
 	}
 }
