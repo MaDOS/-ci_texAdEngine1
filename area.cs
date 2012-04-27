89c67c93bf77b2c5a9cd7041327177543c83a536
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-04-27 18:29:19 +0200

    - added possibility to have more than 1 item of the same type exist in an area - eddited save-algorythm

--- area.cs
+++ area.cs
@@ -6,10 +6,10 @@ namespace ci_texAdEngine1
 {
 	public class area
 	{
-		Dictionary<int, item> items;
-		IniFile dataIni;
-		string filename;
-		string name;
+		public Dictionary<int, item[]> items;
+		private IniFile dataIni;
+		private string filename;
+		private string name;
 		
 		public area(string name)
 		{
@@ -32,24 +32,50 @@ namespace ci_texAdEngine1
 			filename = dataIni.GetSetting("identification", "filename");
 			name = dataIni.GetSetting("identification", "name");
 			
-			items = new Dictionary<int, item>();
+			items = new Dictionary<int, item[]>();
 			
-			string tempItemString = dataIni.GetSetting("content", "items");
+			string searializedItems = dataIni.GetSetting("content", "items");
+			int itemCount;
+			string itemType;
+			string[] tempItemStringSplit;
+			item tempItem;
+			item[] tempItemArray;
 			
-			foreach(string itemFilename in tempItemString.Split(','))
+			foreach(string tempItemString in searializedItems.Split(','))
 			{
-				items.Add(items.Count, new item(itemFilename));
+				tempItemStringSplit = tempItemString.Split('#');
+				itemType = tempItemStringSplit[0];
+				itemCount = Convert.ToInt32(tempItemStringSplit[1]);
+				tempItem = new item(itemType);
+				
+				tempItemArray = new item[itemCount];
+				
+				for(int i = 1; i <= itemCount; i++)
+				{
+					tempItemArray[i] = tempItem;
+				}
+				
+				items.Add(tempItem.id, tempItemArray);
 			}
-			
+		}
+		
+		public void spawnPlayer(string name)
+		{
+			game.activePlayers.Add(name, new player(name, this));
 		}
 		
 		public void save()
 		{
 			string itemsString = "";
 			
-			foreach(item i in items.Values)
+			foreach(int itemId in items.Keys)
 			{
-				itemsString += i.filename + ",";
+				int itemCount = 0;
+				foreach(item i in items[itemId])
+				{
+					itemCount++;
+				}
+				itemsString += items[itemId][0].filename + "#" + itemCount + ",";
 			}
 			
 			itemsString = itemsString.Substring(0, itemsString.Length - 1); //trim last comma away
