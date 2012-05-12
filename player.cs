bfa6b519d01b7fb0def5569a03e2ce3a9916d9c9
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-05-12 14:44:14 +0200

    player class does now handle checking if a player is new. Also player-class does now check if password matches. At least player-class does now save the password encrypted as SHA512 instead of encrypting the whole player-save

--- player.cs
+++ player.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
-using ci_;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ci_texAdEngine1
 {
@@ -9,48 +10,85 @@ namespace ci_texAdEngine1
 	{
 		public Dictionary<int, int> inventory; //id, count
 		public string name;
+		public string password;
+		public bool wasNew;
+		public bool loginOk = false;
 		private IniFile dataIni;
 		private string filename;
-		private area position;
+		public area position;
+		public int areaId;
+		private Dictionary<int, area> areasAvailable;
 		private string saveFile;
 		private int health = 100;
 		
-		public player(string playername, area pos)
+		public player(string playername, string password_param, Dictionary<int, area> areasAvailable_param)
 		{
 			saveFile = game.root + "\\saves\\players\\" + playername + ".ini";
 			
 			inventory = new Dictionary<int, int>();
+			areasAvailable = areasAvailable_param;
 			
 			if(File.Exists(saveFile))
 			{
 				//player exists
-				ci_crypter.encodeFile(saveFile, 139);
+				wasNew = false;
+				
+				Console.WriteLine("Player-save exists loading player " + playername + "...");
+				
 				dataIni = new IniFile(saveFile);
-				ci_crypter.decodeFile(saveFile, 139);
 
 				
 				//attributes
 				health = Convert.ToInt32(dataIni.GetSetting("attributes", "health"));
 				
 				//inventory parsing
-				string serializedItems = dataIni.GetSetting("content", "drops");
-				string[] serializedPairs = serializedItems.Split(',');
-				string[] splittedPair = new string[1];
-				foreach(string pair in serializedPairs)
+				string serializedItems = dataIni.GetSetting("inventory", "items");
+				if(serializedItems.Trim() != "")
 				{
-					splittedPair = pair.Split(':');
-					inventory.Add(Convert.ToInt32(splittedPair[0]), Convert.ToInt32(splittedPair[1]));
+					string[] serializedPairs = serializedItems.Split(',');
+					string[] splittedPair = new string[1];
+					foreach(string pair in serializedPairs)
+					{
+						splittedPair = pair.Split(':');
+						inventory.Add(Convert.ToInt32(splittedPair[0]), Convert.ToInt32(splittedPair[1]));
+					}
 				}
+				
+				name = dataIni.GetSetting("identification", "name");
+				password = dataIni.GetSetting("identification", "password");
+				
+				if(GetSHA512(password_param) == password)
+				{
+					loginOk = true;
+				}
+				
+				areaId = Convert.ToInt32(dataIni.GetSetting("attributes", "areaId"));
+				
+				Console.WriteLine("Done loading player " + name + "...");
 			}
 			else
 			{
 				//player is new on the server
+				Console.WriteLine("No player-save detecded. Creating player " + playername + "...");
+				new StreamWriter(new FileStream(saveFile, FileMode.Create)).Close();
+				dataIni = new IniFile(saveFile);
+				wasNew = true;
 				filename = playername + ".ini";
-				name = playername;
+				name = playername.ToUpper();
+				password = GetSHA512(password_param);
+				areaId = 0;
+				loginOk = true;
+				dataIni.AddSetting("identification", "filename", filename);
+				dataIni.AddSetting("identification", "name", name);
+				dataIni.AddSetting("identification", "password", password);
+				dataIni.AddSetting("attributes", "areaId", areaId.ToString());
+				dataIni.AddSetting("attributes", "health", health.ToString());
+				this.save();
+				Console.WriteLine("Done creating player " + name + "...");
 			}
-			position = pos;
 			
-			this.save();
+			position = areasAvailable[areaId];
+			
 		}
 		
 		public void takeItem(int id)
@@ -75,8 +113,11 @@ namespace ci_texAdEngine1
 		{
 			if(this.position.directions[direction] != -1)
 			{
-				game.areas[this.position.directions[direction]].activePlayers.Add(this.name, this);
-				position.activePlayers.Remove(this.name);
+				area newPos = areasAvailable[position.directions[direction]];
+				
+				position.activePlayers.Remove(name);
+				newPos.activePlayers.Add(name, this);
+				position = newPos;
 			}
 		}
 		
@@ -88,15 +129,32 @@ namespace ci_texAdEngine1
 			{
 				serializedItems += id.ToString() + ":" + inventory[id].ToString() + ",";
 			}
-			
-			serializedItems = serializedItems.Substring(0, serializedItems.Length - 1); //trim last comma away
+				
+			if(serializedItems.Length > 0)
+			{
+				serializedItems = serializedItems.Substring(0, serializedItems.Length - 1); //trim last comma away
+			}
 			
 			dataIni.AddSetting("inventory", "items", serializedItems);
-			dataIni.AddSetting("attributes", "area", position.id.ToString());
-			
-			ci_crypter.encodeFile(saveFile, 139);
+			dataIni.AddSetting("attributes", "areaId", areaId.ToString());
 			dataIni.SaveSettings();
-			ci_crypter.decodeFile(saveFile, 139);
+		}
+		
+		private string GetSHA512(string text)
+		{
+			UnicodeEncoding UE = new UnicodeEncoding();
+			byte[] hashValue;
+			byte[] message = UE.GetBytes(text);
+			 
+			SHA512Managed hashString = new SHA512Managed();
+			string hex = "";
+			 
+			hashValue = hashString.ComputeHash(message);
+			foreach (byte x in hashValue)
+			{
+				hex += String.Format("{0:x2}", x);
+			}
+			return hex.ToUpper();
 		}
 	}
 }
