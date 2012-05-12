30229d9a9ce790ef364599aef262c3452a5db4de
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-05-12 14:39:31 +0200

    rewritten onConnect in a much simplier way, password check is now done by player class

--- game.cs
+++ game.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using ci_;
 using System.Reflection;
 using ci_texAdEngine1;
 
@@ -16,108 +15,53 @@ namespace ci_texAdEngine1
 		public static Dictionary<int, item> items; //id, instance
 		public static Dictionary<int, area> areas; //filename, instance
 		private static IniFile itemIds;
-		     
-        private struct SectionPair
-        {
-            public String Section;
-            public String Key;
-        }
+		private static IniFile areaIds;
 		
 		public game(string map_param)
 		{
+			map = map_param;
+			
 			items = new Dictionary<int, item>();
 			areas = new Dictionary<int, area>();
 			
 			itemIds = new IniFile(root + "\\items\\ids.ini");
 			itemsCount = itemIds.keyPairs.Count;
 			
-			foreach(DictionaryEntry entry in itemIds.keyPairs)
+			Console.WriteLine("Loading items...");
+			
+			foreach(string id in itemIds.EnumSection("ids"))
 			{
-				Console.Write(entry.Key.GetType().ToString() + " => ");
-				Console.WriteLine(entry.Key.GetType().GetFields()[0].);
-				Console.Write(entry.Value.GetType().ToString() + " => ");
-				Console.WriteLine(entry.Value);
-//				SectionPair pair = entry.Value.;
+				string itemFile = itemIds.GetSetting("ids", id);
 				
-//				sectPair.Section = "ids";
-//				sectPair.Key = ((SectionPair)entry.Value).Key;
-//				items.Add(Convert.ToInt32(sectPair.Key), new item(((String)itemIds.keyPairs[sectPair])));
+				items.Add(Convert.ToInt32(id), new item(itemFile));
 				
-//				items.Add(Convert.ToInt32(pair.Key), new item(((String)itemIds.keyPairs[pair])));
+				Console.WriteLine("Loaded item with id: " + id + " => " + itemFile.Replace(".ini", ""));
 			}
 			
-			StreamReader rdr = new StreamReader(root + "\\items\\ids.ini");
-//			string line = rdr.ReadLine();
-//			while(line.Trim() != "")
-//			{
-//				if(line.ToCharArray()[0] == '#'
-//				   || line.ToCharArray()[0] == '\'' 
-//				   || line.ToCharArray()[0] == '[')
-//				{
-//					//comment or section-identifier
-//				}
-//				else
-//				{
-//					string[] splitLine = line.Split('=');
-//					items.Add(Convert.ToInt32(splitLine[0]), new item(splitLine[1]));
-//				}
-//				line = rdr.ReadLine();
-//			}
-//			rdr.Close();
 			
-			rdr = new StreamReader(root + "\\maps\\" + map + "\\ids.ini");
-			string line = rdr.ReadLine();
+			areaIds = new IniFile(root + "\\maps\\" + map + "\\ids.ini");
 			
-			line = rdr.ReadLine();
-			while(line != "")
+			Console.WriteLine("Loading areas of map \"" + map + "\" ...");
+			
+			foreach(string id in areaIds.EnumSection("ids"))
 			{
-				if(line.ToCharArray()[0] == '#' 
-				   || line.ToCharArray()[0] == '\'' 
-				   || line.ToCharArray()[0] == '[')
-				{
-					//comment or section-identifier
-				}
-				else
-				{
-					string[] splitLine = line.Split('=');
-					areas.Add(Convert.ToInt32(splitLine[0]), new area(splitLine[1]));
-				}
-				line = rdr.ReadLine();
+				string areaFile = areaIds.GetSetting("ids", id);
+				
+				areas.Add(Convert.ToInt32(id), new area(map, areaFile));
+				
+				Console.WriteLine("Loaded area with id: " + id + " => " + areaFile.Replace(".ini", ""));
 			}
-			rdr.Close();
 		}
 		
-		public string onConnect(string playername, string password) //first connection handling and password protecting are a bit messed up and very unsafe. I will change this later
-		{
-			string saveFile = root + "\\saves\\players\\" + playername + ".ini";
-			
-			if(File.Exists(saveFile))
-			{
-			
-				ci_crypter.encodeFile(saveFile, 139);
-				IniFile playerDataIni = new IniFile(saveFile);
-				ci_crypter.decodeFile(saveFile, 139);
-					
-				//check password
-				if(password != playerDataIni.GetSetting("identification", "password"))
-				{
-					return "incorrect password!";
-				}
-				
-				int spawnAreaId = Convert.ToInt32(playerDataIni.GetSetting("attributes", "area"));
-				
-				areas[spawnAreaId].activePlayers.Add(playername, new player(playername, areas[spawnAreaId]));
-			}
-			else
-			{
-				IniFile playerDataIni = new IniFile(saveFile);
-				playerDataIni.AddSetting("identification", "password", password);
-				playerDataIni.SaveSettings();
-				ci_crypter.decodeFile(saveFile, 139);
-				areas[0].activePlayers.Add(playername, new player(playername, areas[0]));
-			}
-			
-			return "connected successfully!";
+		
+        /// <summary>
+        /// Method to call to pass a connected client to the engine. Returns: false if password was wrong.
+        /// </summary>
+        /// <param name="connectedPlayer">Instance of the player that connected.</param>
+		public void onConnect(player connectedPlayer)
+		{	
+			Console.WriteLine("Spawning player " + connectedPlayer.name + " in area " + connectedPlayer.position.name + " (areaId: " + connectedPlayer.areaId + ")");
+			connectedPlayer.position.activePlayers.Add(connectedPlayer.name, connectedPlayer);
 		}
 	}
 }
